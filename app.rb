e23a68efd22e9db90f28c9a78a83c93be1f5497d
Sidneyyy <catitude14@outlook.com>
2018-07-17 13:56:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+ get '/' do
+   "Hello, World!"
+ end
+ 
+ get '/name' do
+  "My name is _"
+end
+  
+  get ''
+  
+  
+  
 end
